455910651d3d0538a927b8a003d3b43f3d537933
seshan03 <seshanprof@gmail.com>
2026-07-27 21:21:08 +0530

    ci: add CI pipeline workflow and eslint config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+      },
+    },
+    rules: {},
+  },
+];
