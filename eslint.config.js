498e9c144561275b64fa38098c5d2ab30cc9cecc
seshan03 <seshanprof@gmail.com>
2026-07-27 21:36:54 +0530

    fix: enable JSX parsing in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,15 @@ import js from '@eslint/js';
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         process: 'readonly',
         console: 'readonly',
